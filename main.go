38cd916787757913a9264cc5f90a491c5ba2e979
William Walker <w_walker@icloud.com>
2024-05-24 09:07:02 -0500

    new var names; save tokens to keychain

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"os/user"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/textinput"
@@ -17,40 +20,40 @@ var (
 )
 
 // Model representing the application state
-type model struct {
-	key   textinput.Model
-	value textinput.Model
+type appModel struct {
+	tokenNameInput  textinput.Model
+	tokenValueInput textinput.Model
 }
 
 // Initialize the input model with two text inputs
-func createModel() model {
-	key := textinput.New()
-	key.Placeholder = "key"
-	key.CharLimit = 32
-	key.Focus()
-	key.Cursor.Style = focusedStyle
-	key.PromptStyle = focusedStyle
-	key.TextStyle = focusedStyle
+func newModel() appModel {
+	tokenNameInput := textinput.New()
+	tokenNameInput.Placeholder = "Token Key"
+	tokenNameInput.CharLimit = 32
+	tokenNameInput.Focus()
+	tokenNameInput.Cursor.Style = focusedStyle
+	tokenNameInput.PromptStyle = focusedStyle
+	tokenNameInput.TextStyle = focusedStyle
 
-	value := textinput.New()
-	value.Placeholder = "value"
-	value.CharLimit = 128
-	value.EchoMode = textinput.EchoPassword
-	value.EchoCharacter = '•'
-	value.PromptStyle = blurredStyle
-	value.TextStyle = blurredStyle
-	value.Cursor.Style = blurredStyle
+	tokenValueInput := textinput.New()
+	tokenValueInput.Placeholder = "Token Value"
+	tokenValueInput.CharLimit = 128
+	tokenValueInput.EchoMode = textinput.EchoPassword
+	tokenValueInput.EchoCharacter = '•'
+	tokenValueInput.PromptStyle = blurredStyle
+	tokenValueInput.TextStyle = blurredStyle
+	tokenValueInput.Cursor.Style = blurredStyle
 
-	return model{key: key, value: value}
+	return appModel{tokenNameInput: tokenNameInput, tokenValueInput: tokenValueInput}
 }
 
 // Initialize the application
-func (m model) Init() tea.Cmd {
+func (m appModel) Init() tea.Cmd {
 	return textinput.Blink
 }
 
 // Update the application state based on user input
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -68,17 +71,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 // Handle navigation keys and focus changes
-func handleNavigation(key string, m *model) bool {
-	if key == "enter" && m.value.Focused() {
-		return saveInputs(m)
+func handleNavigation(key string, m *appModel) bool {
+	if key == "enter" && m.tokenValueInput.Focused() {
+		return saveToken(m)
 	}
 
 	if key == "up" || key == "shift+tab" {
-		m.key.Focus()
-		m.value.Blur()
+		m.tokenNameInput.Focus()
+		m.tokenValueInput.Blur()
 	} else {
-		m.value.Focus()
-		m.key.Blur()
+		m.tokenValueInput.Focus()
+		m.tokenNameInput.Blur()
 	}
 
 	updateStyles(m)
@@ -86,51 +89,63 @@ func handleNavigation(key string, m *model) bool {
 }
 
 // Update styles based on focus
-func updateStyles(m *model) {
-	if m.key.Focused() {
-		m.key.PromptStyle = focusedStyle
-		m.key.TextStyle = focusedStyle
-		m.key.Cursor.Style = focusedStyle
-		m.value.PromptStyle = blurredStyle
-		m.value.TextStyle = blurredStyle
-		m.value.Cursor.Style = blurredStyle
+func updateStyles(m *appModel) {
+	if m.tokenNameInput.Focused() {
+		m.tokenNameInput.PromptStyle = focusedStyle
+		m.tokenNameInput.TextStyle = focusedStyle
+		m.tokenNameInput.Cursor.Style = focusedStyle
+		m.tokenValueInput.PromptStyle = blurredStyle
+		m.tokenValueInput.TextStyle = blurredStyle
+		m.tokenValueInput.Cursor.Style = blurredStyle
 	} else {
-		m.value.PromptStyle = focusedStyle
-		m.value.TextStyle = focusedStyle
-		m.value.Cursor.Style = focusedStyle
-		m.key.PromptStyle = blurredStyle
-		m.key.TextStyle = blurredStyle
-		m.key.Cursor.Style = blurredStyle
+		m.tokenValueInput.PromptStyle = focusedStyle
+		m.tokenValueInput.TextStyle = focusedStyle
+		m.tokenValueInput.Cursor.Style = focusedStyle
+		m.tokenNameInput.PromptStyle = blurredStyle
+		m.tokenNameInput.TextStyle = blurredStyle
+		m.tokenNameInput.Cursor.Style = blurredStyle
 	}
 }
 
 // Update input fields based on user input
-func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+func (m *appModel) updateInputs(msg tea.Msg) tea.Cmd {
 	cmds := make([]tea.Cmd, 2)
-	m.key, cmds[0] = m.key.Update(msg)
-	m.value, cmds[1] = m.value.Update(msg)
+	m.tokenNameInput, cmds[0] = m.tokenNameInput.Update(msg)
+	m.tokenValueInput, cmds[1] = m.tokenValueInput.Update(msg)
 	return tea.Batch(cmds...)
 }
 
-// Save the value to the keychain
-func saveInputs(m *model) bool {
-	fmt.Printf("%s=%s\n", m.key.Value(), m.value.Value())
+// Save the token value to the keychain
+func saveToken(m *appModel) bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	username := currentUser.Username
+
+	cmd := exec.Command("security", "add-generic-password", "-a", username, "-s", m.tokenNameInput.Value(), "-w", m.tokenValueInput.Value())
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Failed to save token to keychain: %s\n", err)
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("%s=%s\n", m.tokenNameInput.Value(), m.tokenValueInput.Value())
 	return true
 }
 
 // Render the view
-func (m model) View() string {
+func (m appModel) View() string {
 	var b strings.Builder
-	b.WriteString(m.key.View())
+	b.WriteString(m.tokenNameInput.View())
 	b.WriteRune('\n')
-	b.WriteString(m.value.View())
+	b.WriteString(m.tokenValueInput.View())
 	b.WriteRune('\n')
 	return b.String()
 }
 
 // Main function to run the application
 func main() {
-	if _, err := tea.NewProgram(createModel()).Run(); err != nil {
+	if _, err := tea.NewProgram(newModel()).Run(); err != nil {
 		fmt.Printf("could not start program: %s\n", err)
 		os.Exit(1)
 	}
